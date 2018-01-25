75a8120f637eb890601d4a3381a2a269660d342f
cat <15031937570@163.com>
2018-01-25 10:50:31 +0800

    td_speed

--- Monitor_v3/CommandQuery/views.py
+++ Monitor_v3/CommandQuery/views.py
@@ -16,8 +16,8 @@ def CommandQuery(req):
         print("开始时间："+str(NowTime))
         command = req.REQUEST.get('command','0')
         print("---command=-------"+command+"----------")
-        username = 'bjywb'
-        pkey_file='/home/bjywb/.ssh/hskj_20130620_bjywb'
+        username = 'root'
+        pkey_file='/root/.ssh/id_rsa'
         ip_file = '/hskj/script/ip.txt'
         table_list = []
         f = file(ip_file)
@@ -31,7 +31,7 @@ def CommandQuery(req):
                 s = paramiko.SSHClient()
                 s.load_system_host_keys()
                 s.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
-                key = paramiko.RSAKey.from_private_key_file(pkey_file,'&U*I(O1208')
+                key = paramiko.RSAKey.from_private_key_file(pkey_file,'666666')
                 s.connect(server,port,username,pkey=key,timeout=10)
                 stdin,stdout,stderr = s.exec_command(command)
                 for result in stdout.readlines():
@@ -72,8 +72,8 @@ def clusterSubmit(req):
                 logName_sgip = "/hskj/logs/sgip_info.log."+startTime
                 logName_smgp = "/hskj/logs/smgp_info.log."+startTime
         command = "grep -a  '"+search1+"' "+logName_http+" "+logName_cmpp+" "+logName_sgip+" "+logName_smgp+"|grep -a '"+startTime+" "+hour+"' | grep -a '"+search2+"'|grep -v -a '\-\-'|grep -v 'send test'|grep -v 'receive test'|grep -v 'report'"
-        username = 'bjywb'
-        pkey_file='/home/bjywb/.ssh/hskj_20130620_bjywb'
+        username = 'root'
+        pkey_file='/root/.ssh/id_rsa'
         table_list = []
         server_list ={}
         if type == "cluster_64":
@@ -191,7 +191,29 @@ def clusterSubmit(req):
                             result=re.sub(search2,'<strong style="color:red;">'+search2+'</strong>',result)
                         result=re.sub('return','<strong style="color:red;">return</strong>',result)
                         table_list.append({'server':server_ip,'content':result})
-            
+        if type == "cluster_local":
+            server_list = {"c6_1":["192.168.81.203",22],"c6_2":["192.168.81.203",23]}
+            for line in server_list.values():
+                    server = line[0]
+                    port = line[1]
+                    if port == 22:
+                        server_ip = "192.168.81.203"
+                    if port == 23:
+                        server_ip = "192.168.81.203"
+                    s = paramiko.SSHClient()
+                    s.load_system_host_keys()
+                    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                    key = paramiko.RSAKey.from_private_key_file(pkey_file,'666666')
+                    ##s.connect(server,'22',username,pkey=key,timeout=30)
+                    s.connect(server,22,username,pkey=key,timeout=10)
+                    stdin,stdout,stderr = s.exec_command(command)
+                    for result in stdout.readlines():
+                        result=re.sub(search1,'<strong style="color:red;">'+search1+'</strong>',result)
+                        if search2 != "":
+                            result=re.sub(search2,'<strong style="color:red;">'+search2+'</strong>',result)
+                        result=re.sub('return','<strong style="color:red;">return</strong>',result)
+                        table_list.append({'server':server_ip,'content':result})
+
         NowTime = time.strftime('%Y-%m-%d %H:%M:%S')
         return render_to_response("Cluster_Submit.html",locals())
     else:
@@ -563,116 +585,63 @@ def request_ip(req):
 
 def td_speed(req):
     if req.method == 'POST':
-        type = req.REQUEST.get('type','cluster_64')
-        pro = req.REQUEST.get('pro','http')
-        user_id = req.REQUEST.get('user_id','')
-        if pro == 'http':
-            command="grep -a '"+user_id+"' /hskj/logs/httpPostLog.log |awk -F[\;:]+ '{print $6}'|sort|uniq"
-        elif pro == 'cmpp':
-            command="grep -a '"+user_id+"' /hskj/logs/cmpp_info.log |grep -a 'submitResp'|awk -F[' ':]+ '{print $(NF-3)}'|sort|uniq"
-        elif pro == 'sgip':
-            command="grep -a '"+user_id+"' /hskj/logs/sgip_info.log |grep -a 'return_code'|awk  '{print $9}'|sort|uniq"
+        NowTime = time.strftime('%Y-%m-%d %H:%M:%S')
+        td_code = req.REQUEST.get('td_code','0')
+        search2 = req.REQUEST.get('search2','0')
+        hour = req.REQUEST.get('hour','')
+        type = req.REQUEST.get('type','cluster_local')
+        nowDate = time.strftime('%Y-%m-%d')
+        startTime = req.REQUEST.get('startTime',nowDate)
+        nowDate = time.strftime('%Y-%m-%d')
+        logName_http=""
+        logName_cmpp=""
+        if startTime == "":
+            startTime = nowDate
+            logName_http = "/hskj/logs/info.log"
+            logName_cmpp = "/hskj/logs/cmpp_info.log"
+            logName_sgip = "/hskj/logs/sgip_info.log"
+            logName_smgp = "/hskj/logs/smgp_info.log"
         else:
-            command="grep -a '"+user_id+"' /hskj/logs/smgp_info.log |grep -a 'return_code'|awk  '{print $9}'|sort|uniq"
-        username = 'bjywb'
-        pkey_file='/home/bjywb/.ssh/hskj_20130620_bjywb'
+            if startTime == nowDate:
+                logName_http = "/hskj/logs/info.log"
+                logName_cmpp = "/hskj/logs/cmpp_info.log"
+                logName_sgip = "/hskj/logs/sgip_info.log"
+                logName_smgp = "/hskj/logs/smgp_info.log"
+            else:
+                logName_http = "/hskj/logs/info.log."+startTime
+                logName_cmpp = "/hskj/logs/cmpp_info.log."+startTime
+                logName_sgip = "/hskj/logs/sgip_info.log."+startTime
+                logName_smgp = "/hskj/logs/smgp_info.log."+startTime
+        ##command = "grep -a  '"+search1+"' "+logName_http+" "+logName_cmpp+" "+logName_sgip+" "+logName_smgp+"|grep -a '"+startTime+" "+hour+"' | grep -a '"+search2+"'|grep -v -a '\-\-'|grep -v 'send test'|grep -v 'receive test'|grep -v 'report'"
+        command = "for i in {"+logName_http+","+logName_cmpp+","+logName_sgip+","+logName_smgp+"};do tail -10000 $i;done | grep -a 'yw_code: "+td_code+"' |grep -a '"+nowDate+" "+hour+"'| grep -a \"waitSubmitRespMap\" | awk -F \",\" '{print $1}' | sort | uniq -c "
+        username = 'root'
+        pkey_file='/root/.ssh/id_rsa'
         table_list = []
         server_list ={}
-        if type == "cluster_64":
-            server_list = {"c1_56":["210.14.134.81",10056],"c1_57":["210.14.134.81",10057],"c1_58":["210.14.134.81",10058]}
+        if type == "cluster_local":
+            server_list = {"c6_1":["192.168.81.203",22,"c6_1"],"c6_2":["192.168.81.203",23,"c6_2"]}
             for line in server_list.values():
+                    sender_code=line[2]   
                     server = line[0]
                     port = line[1]
-                    if port == 10056:
-                        server_ip = "172.16.10.56"
-                    if port == 10057:
-                        server_ip = "172.16.10.57"
-                    if port == 10058:
-                        server_ip = "172.16.10.58"
+                    if port == 22:
+                        server_ip = "192.168.81.203"
+                    if port == 23:
+                        server_ip = "192.168.81.203"
                     s = paramiko.SSHClient()
                     s.load_system_host_keys()
-                    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    key = paramiko.RSAKey.from_private_key_file(pkey_file,'&U*I(O1208')
-                    s.connect(server,port,username,pkey=key,timeout=10)
+                    s.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+                    key = paramiko.RSAKey.from_private_key_file(pkey_file,'666666')
+                    s.connect(server,22,username,pkey=key,timeout=10)
                     stdin,stdout,stderr = s.exec_command(command)
                     for result in stdout.readlines():
-                        table_list.append({'server':server_ip,'content':result})
-        if type == "cluster_227":
-            server_list = {"c2_24":["202.108.253.227",10024],"c2_25":["202.108.253.227",10025],"c2_33":["202.108.253.227",10033],"c2_34":["202.108.253.227",10034]}
-            for line in server_list.values():
-                    server = line[0]
-                    port = line[1]
-                    if port == 10024:
-                        server_ip = "172.17.90.24"
-                    if port == 10025:
-                        server_ip = "172.17.90.25"
-                    if port == 10033:
-                        server_ip = "172.17.90.33"
-                    if port == 10034:
-                        server_ip = "172.17.90.34"
-                    s = paramiko.SSHClient()
-                    s.load_system_host_keys()
-                    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    key = paramiko.RSAKey.from_private_key_file(pkey_file,'&U*I(O1208')
-                    s.connect(server,port,username,pkey=key,timeout=10)
-                    stdin,stdout,stderr = s.exec_command(command)
-                    for result in stdout.readlines():
-                        table_list.append({'server':server_ip,'content':result})
-        if type == "cluster_35":
-            server_list = {"c3_61":["36.110.168.35",10061],"c3_62":["36.110.168.35",10062],"c3_63":["36.110.168.35",10063]}
-            for line in server_list.values():
-                    server = line[0]
-                    port = line[1]
-                    if port == 10061:
-                        server_ip = "172.17.130.61"
-                    if port == 10062:
-                        server_ip = "172.17.130.62"
-                    if port == 10063:
-                        server_ip = "172.17.130.63"
-                    s = paramiko.SSHClient()
-                    s.load_system_host_keys()
-                    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    key = paramiko.RSAKey.from_private_key_file(pkey_file,'&U*I(O1208')
-                    s.connect(server,port,username,pkey=key,timeout=10)
-                    stdin,stdout,stderr = s.exec_command(command)
-                    for result in stdout.readlines():
-                        table_list.append({'server':server_ip,'content':result})
-        if type == "cluster_226":
-            server_list = {"c4_61":["111.13.124.226",10061],"c4_62":["111.13.124.226",10062],"c4_63":["111.13.124.226",10063]}
-            for line in server_list.values():
-                    server = line[0]
-                    port = line[1]
-                    if port == 10061:
-                        server_ip = "172.17.140.61"
-                    if port == 10062:
-                        server_ip = "172.17.140.62"
-                    if port == 10063:
-                        server_ip = "172.17.140.63"
-                    s = paramiko.SSHClient()
-                    s.load_system_host_keys()
-                    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    key = paramiko.RSAKey.from_private_key_file(pkey_file,'&U*I(O1208')
-                    s.connect(server,port,username,pkey=key,timeout=10)
-                    stdin,stdout,stderr = s.exec_command(command)
-                    for result in stdout.readlines():
-                        table_list.append({'server':server_ip,'content':result})
-        if type == "cluster_21":
-            server_list = {"c5_61":["222.73.121.21",10061],"c5_62":["222.73.121.21",10062]}
-            for line in server_list.values():
-                    server = line[0]
-                    port = line[1]
-                    if port == 10061:
-                        server_ip = "172.17.30.61"
-                    if port == 10062:
-                        server_ip = "172.17.30.62"
-                    s = paramiko.SSHClient()
-                    s.load_system_host_keys()
-                    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    key = paramiko.RSAKey.from_private_key_file(pkey_file,'&U*I(O1208')
-                    s.connect(server,port,username,pkey=key,timeout=10)
-                    stdin,stdout,stderr = s.exec_command(command)
-                    for result in stdout.readlines():
-                        table_list.append({'server':server_ip,'content':result})
+                        ##result=re.sub(td_code,'<strong style="color:red;">'+td_code+'</strong>',result)
+                        result=re.sub(td_code,'',result)
+                        if search2 != "":
+                            result=re.sub(search2,''+search2+'',result)
+                        result=re.sub('return','',result)
+                        table_list.append({'server':server_ip,'content':result,'sender':sender_code})
+        NowTime = time.strftime('%Y-%m-%d %H:%M:%S')
         return render_to_response("td_speed.html",locals())
     else:
-        return render_to_response('td_speed.html',locals())
+        return render_to_response("td_speed.html",locals())
